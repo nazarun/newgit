c01b82c65d8ed26b84795274db16689eaef36af9
Nazar <nazarun@gmail.com>
2017-12-11 09:13:00 +0200

    class Area

--- task-areas.js
+++ task-areas.js
@@ -16,3 +16,34 @@ function Circle(r){
 		return Pi * this.r * this.r;
 	}
 }
+
+//class Area
+function Area(){
+	this._figures = [];
+	
+	this.addFigure = function(figure){
+		this._figures.push(figure);
+	};
+	this.clear = function(){
+		this._figures = [];
+	};
+	Object.defineProperty(this, "size", {
+		get: function(){
+			var areaSum = 0;
+			for(var x=0; x<this._figures.length; x++){
+			areaSum	+= this._figures[x].getArea();
+			};			
+			return areaSum;
+		}
+	});
+}
+
+//testing
+var figure1 = new Square(10, 15);
+var figure2 = new Square(15, 15);
+var circle1 = new Circle(15);
+var area = new Area();
+area.addFigure(figure1);
+area.addFigure(figure2);
+area.addFigure(circle1);
+console.log("Size: ", area.size);

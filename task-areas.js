c23255536a095b70a579adc2c9ead58ec733635d
Nazar <nazarun@gmail.com>
2017-12-11 09:11:12 +0200

    class Circle

--- task-areas.js
+++ task-areas.js
@@ -6,3 +6,13 @@ function Square(width, height){
 		return this.width * this.height;
 	}
 }
+
+//class Circle
+function Circle(r){
+	Square.apply(this, arguments);
+	this.r = r;
+	const Pi = 3.14;
+	this.getArea = function(){
+		return Pi * this.r * this.r;
+	}
+}
